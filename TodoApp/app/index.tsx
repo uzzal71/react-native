2c4eab26ad2042f8d4c5c79b222bf7b4e4affcaf
uzzal71 <uzzalroy.acm@gmail.com>
2025-08-09 23:36:31 +0600

    Added header section

--- TodoApp/app/index.tsx
+++ TodoApp/app/index.tsx
@@ -1,15 +1,88 @@
-import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const todoItems = [
+    { id: 1, title: "Buy groceries", isDone: false },
+    { id: 2, title: "Finish React Native project", isDone: false },
+    { id: 3, title: "Call the dentist", isDone: true },
+    { id: 4, title: "Clean the kitchen", isDone: false },
+    { id: 5, title: "Read a book", isDone: true },
+    { id: 6, title: "Go for a walk", isDone: false },
+    { id: 7, title: "Reply to emails", isDone: false },
+    { id: 8, title: "Prepare dinner", isDone: true },
+    { id: 9, title: "Do laundry", isDone: false },
+    { id: 10, title: "Water the plants", isDone: false },
+  ];
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome to Todo App.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={() => {
+            alert("Clicked!");
+          }}
+        >
+          <Ionicons name="menu" size={24} color={"#333"} />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => {}}>
+          <Image
+            source={{ uri: "https://xsgames.co/randomusers/avatar.php?g=male" }}
+            style={{ width: 40, height: 40, borderRadius: 20 }}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.searchBar}>
+        <Ionicons name="search" size={24} color={"#333"} />
+        <TextInput
+          placeholder="Search"
+          style={styles.searchInput}
+          clearButtonMode="always"
+        />
+      </View>
+      <FlatList
+        data={todoItems}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={{ paddingVertical: 10 }}>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, paddingHorizontal: 20, backgroundColor: "#f5f5f5" },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  searchBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 5,
+    borderRadius: 10,
+    gap: 10,
+    marginBottom: 20,
+  },
+  searchInput: {
+    flex: 1,
+    fontSize: 16,
+    color: "#333",
+  },
+});
